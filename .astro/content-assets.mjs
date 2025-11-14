1808c71a61f2035ab208cb7cbf6673a126e88b4a
VyomJain6904 <vyom.malo6904@gmail.com>
2025-11-14 11:21:25 +0530

    Remove the Astro Stack

--- .astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_Z1RvI1I from "./Cap/image.png?astroContentImageFlag=&importer=src%2Fcontents%2Fposts%2Fcap-htb.md";
-export default new Map([["./Cap/image.png?astroContentImageFlag=&importer=src%2Fcontents%2Fposts%2Fcap-htb.md", __ASTRO_IMAGE_IMPORT_Z1RvI1I]]);
-		
